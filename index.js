ed1b6684b88de02605d79d0b0324b48afa7346ad
Izzat-Hasan <izzathasan03@gmail.com>
2022-02-19 12:53:36 -0600

    idk

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var http = require('http');
 var url = require('url');
-router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'index.html'));
-    //__dirname : It will resolve to your project folder.
-  });
+var dt = require('./index.html');
 
 
 const server = http.createServer((request, response) => {
     // Write the request to the log. 
     console.log(request);
 
-
+    // Standard Hello World.
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('<h3>Hello World!</h3>')
 
 
 
