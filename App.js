9d35c088de5045083f43d6d0c4f63ac1ba35ac1f
Izzat-Hasan <izzathasan03@gmail.com>
2022-02-20 00:08:43 -0600

    d

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const http = require('http');
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const route = require('./routes');
+const route = require('routes');
 const app = express();
 app.use(bodyParser.urlencoded({extended: false})); app.use(express.static(path.join(__dirname, 'public')));
 app.use('/test',route);
